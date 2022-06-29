132d67114fca36ed7fda060691d61cd8655fa18c
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-06-29 17:35:30 -0500

    init

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home.index');
+
+
+Route::get('/contact', function () {
+    return "Contacto";
+})->name('home.contact');
+
+
+Route::get('/posts/{id}', function ($id) {
+    return "Blog post " . $id;
+})->name('posts.show');
+
+
+Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
+    return "Posts from " . $daysAgo;
+})->name('posts.recent.index');

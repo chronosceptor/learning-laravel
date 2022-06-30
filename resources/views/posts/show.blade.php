a04de8f24f77c9aedca92de798c000bdfaa6cb72
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-06-29 20:49:03 -0500

    conditional rendering

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,13 @@
 @section('title', $post['title'])
 
 @section('content')
+
+    @if($post['is_new'])
+        <div>Its new</div>
+    @else
+        <div>Its old</div>
+    @endif
+
     <h1>{{ $post['title'] }}</h1>
     <p>{{ $post['content'] }}</p>
 @endsection

8683107c2cc8cf4d1ac86244d3b310abb494018a
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-06-29 20:39:09 -0500

    add new views

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title','Home Page')
+
+@section('content')
+    <h1>hola desde el index</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index', []);
 })->name('home.index');
 
 
 Route::get('/contact', function () {
-    return "Contacto";
+    return view('home.contact', []);
 })->name('home.contact');
 
 
 Route::get('/posts/{id}', function ($id) {
-    return "Blog post " . $id;
+    $posts = [
+        1 => [
+            'title' => 'Intro to Laravel',
+            'content' => 'This is a short intro to Laravel'
+        ],
+        2 => [
+            'title' => 'Intro to PHP',
+            'content' => 'This is a short intro to PHP'
+        ],
+    ];
+
+    abort_if( !isset($posts[$id]), 404 );
+
+    return view('posts.show', ['post' => $posts[$id]]);
 })->name('posts.show');
 
 

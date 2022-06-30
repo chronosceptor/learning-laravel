f925972df71051e5dd98f3bf3a74fd053ee056ab
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-06-30 12:46:00 -0500

    @unless + @isset

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,6 +10,16 @@
         <div>Its old</div>
     @endif
 
+    @unless($post['is_new'])
+        <div>Its an old post using unless</div>
+    @endunless
+
     <h1>{{ $post['title'] }}</h1>
     <p>{{ $post['content'] }}</p>
+
+    @isset(  $post['has_comments'] )
+        This post has comments using isset
+    @endisset
+
+
 @endsection

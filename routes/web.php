450eadcdf2959c454fea600c6667b1618a3f9050
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-07-02 11:40:31 -0500

    resource controller index + show

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -33,77 +34,59 @@ Route::get('/contact', [HomeController::class, 'contact'])
 
 Route::get('/single', AboutController::class);
 
-
-// list of posts
-$posts = [
-    1 => [
-        'title' => 'Intro to Laravel',
-        'content' => 'This is a short intro to Laravel',
-        'is_new' => true,
-        'has_comments' => true
-    ],
-    2 => [
-        'title' => 'Intro to PHP',
-        'content' => 'This is a short intro to PHP',
-        'is_new' => false
-    ],
-    3 => [
-        'title' => 'Intro to Golang',
-        'content' => 'This is a short intro to Golang',
-        'is_new' => false
-    ],
-];
+Route::resource('posts', PostsController::class)->only(['index','show']);
 
 
-Route::get('/posts', function () use ($posts) {
-    // dd(request()->all());
-    dd((int)request()->query('limit', 1 ));
-    dd((int)request()->input('limit', 1 ));
-    return view('posts.index', ['posts' => $posts]);
-})->name('posts.index');
+
+// Route::get('/posts', function () use ($posts) {
+//     // dd(request()->all());
+//     dd((int)request()->query('limit', 1 ));
+//     dd((int)request()->input('limit', 1 ));
+//     return view('posts.index', ['posts' => $posts]);
+// })->name('posts.index');
 
 
-Route::get('/posts/{id}', function ($id) use ($posts) {
-    abort_if( !isset($posts[$id]), 404 );
-    return view('posts.show', ['post' => $posts[$id]]);
-})->name('posts.show');
+// Route::get('/posts/{id}', function ($id) use ($posts) {
+//     abort_if( !isset($posts[$id]), 404 );
+//     return view('posts.show', ['post' => $posts[$id]]);
+// })->name('posts.show');
 
 
-Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
-    return "Posts from " . $daysAgo;
-})->name('posts.recent.index');
+// Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
+//     return "Posts from " . $daysAgo;
+// })->name('posts.recent.index');
 
 
-Route::prefix('/fun')->name('fun.')->group( function() use($posts) {
+// Route::prefix('/fun')->name('fun.')->group( function() use($posts) {
 
-    Route::get('/responses', function() use($posts){
-        return response($posts, 201)
-            ->header('Content-Type', 'application/json')
-            ->cookie('MY_COOKIE','Chronosceptor', 3600);
-    })->name('responses');
+//     Route::get('/responses', function() use($posts){
+//         return response($posts, 201)
+//             ->header('Content-Type', 'application/json')
+//             ->cookie('MY_COOKIE','Chronosceptor', 3600);
+//     })->name('responses');
 
-    Route::get('/redirect', function() {
-        return redirect('contact');
-    })->name('redirect');
+//     Route::get('/redirect', function() {
+//         return redirect('contact');
+//     })->name('redirect');
 
-    Route::get('/named-redirect', function() {
-        return redirect()->route('posts.show', ['id' => 1]);
-    })->name('named');
+//     Route::get('/named-redirect', function() {
+//         return redirect()->route('posts.show', ['id' => 1]);
+//     })->name('named');
 
-    Route::get('/away', function() {
-        return redirect()->away('https://chronosceptor.com');
-    })->name('away');
+//     Route::get('/away', function() {
+//         return redirect()->away('https://chronosceptor.com');
+//     })->name('away');
 
-    Route::get('/back', function() {
-        return back();
-    })->name('back');
+//     Route::get('/back', function() {
+//         return back();
+//     })->name('back');
 
-    Route::get('/json', function() use($posts) {
-        return response()->json($posts);
-    })->name('json');
+//     Route::get('/json', function() use($posts) {
+//         return response()->json($posts);
+//     })->name('json');
 
-    Route::get('/download', function() {
-        return response()->download("robots.txt");
-    })->name('download');
+//     Route::get('/download', function() {
+//         return response()->download("robots.txt");
+//     })->name('download');
 
-});
+// });

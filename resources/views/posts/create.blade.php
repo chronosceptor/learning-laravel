354de0efb50c1b71eb0625e5f20c0f457801e6f2
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-07-05 15:26:06 -0500

    add errors

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,11 +9,32 @@
     <div>
         <label>Title</label><br>
         <input type="text" name="title">
+        @error('title')
+            <div>
+                {{ $message }}
+            </div>
+        @enderror
     </div>
     <div>
         <label>Content</label><br>
         <textarea name="content"></textarea>
+        @error('content')
+            <div>
+                {{ $message }}
+            </div>
+        @enderror
     </div>
+
+    @if($errors->any())
+        <div>
+            <ul>
+                @foreach( $errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div>
         <input type="submit" value="Create">
     </div>
